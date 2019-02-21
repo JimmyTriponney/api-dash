546db2dd0fc475ed4e1ec011b24cfc8d946590b8
jimmy triponney <jimmy.triponney@yahoo.fr>
2019-02-22 00:38:32 +0100

    Mise en place de l'entité user

---
+++ src/Form/AdminRegistrationType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Admin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminRegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('username')
+            ->add('password')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('email')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Admin::class,
+        ]);
+    }
+}
